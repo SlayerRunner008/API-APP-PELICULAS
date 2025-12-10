83cd6bbaf6a71d5a120835853290ace333c2fefe
Mariano Jara <mjaravillasana@gmail.com>
2025-12-10 11:51:17 -0600

    cambio a la base de datos pa que no se petatie con el render lol

--- config/database.py
+++ config/database.py
@@ -1,19 +1,13 @@
-import os 
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm.session import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+# Render te da la variable DATABASE_URL en el dashboard de la base
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 
-sqlite_file_name= "../database.sqlite"
+engine = create_engine(DATABASE_URL, echo=True)
 
-#estamos leyendo el directorio actual que es database.py
-base_dir = os.path.dirname(os.path.realpath(__file__))
-
-"creamos la url de la base de datos uniendo las 2 variables anteriores"
-database_url = f"sqlite:///{os.path.join(base_dir,sqlite_file_name)}"
-
-engine = create_engine(database_url, echo = True)
-
-Session = sessionmaker(bind=engine)
+Session = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
 Base = declarative_base()
